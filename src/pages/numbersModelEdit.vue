f5e42fa3aeff2bc8cd5706eb216dc34b06385d09
hgcrown <847842399@qq.com>
2019-06-11 15:14:30 +0800

    头部全部居中

--- src/pages/numbersModelEdit.vue
+++ src/pages/numbersModelEdit.vue
@@ -18,9 +18,9 @@
     </div>
     <div class="main_container">
       <div class="model_part">
-        <img src = '../assets/images/name.png'  style="width:50px;height:50px;margin-right: 20px;">
+        <img class="model_type" src = '../assets/images/name.png'  style="width:50px;height:50px;margin-right: 20px;">
         <span class="model_type">{{modelName}}</span>
-        <img src = '../assets/images/type.png'  style="width:50px;height:50px;margin-left: 100px;margin-right: 20px">
+        <img class="model_type" src = '../assets/images/type.png'  style="width:50px;height:50px;margin-left: 100px;margin-right: 20px">
         <span class="model_type">数字</span>
       </div>
       <div class="top_train_block">
@@ -1102,10 +1102,9 @@
     margin-right: 150px;
   }
   .model_type{
-    position: relative;
     font-size:30px;
     line-height:50px;
-    display:inline-block;
+    float:left;
   }
   .notes{
     margin-left: 10px;
